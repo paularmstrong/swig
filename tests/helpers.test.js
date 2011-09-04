da0b6acaeda96a5debebf5c42a9f8a746f56621d
Paul Armstrong <paul@paularmstrongdesigns.com>
2011-09-04 10:09:49 -0700

    doing some cleanup on helpers. adding tests and fixing some.

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -1,21 +1,66 @@
 var helpers = require('../lib/helpers');
 
-exports.clone = function (test) {
-    var obj = { foo: 1, bar: 2, baz: { bop: 3 } },
-        out = helpers.clone(obj);
+exports.isLiteral = function (test) {
+    test.ok(helpers.isLiteral('0.1'), '"0.1" should be a valid literal');
+    test.ok(helpers.isLiteral('"foobar"'), '"foobar" should be a valid literal');
+    test.ok(helpers.isLiteral("'foobar'"), '\'foobar\' should be a valid literal');
 
-    test.deepEqual(out, { foo: 1, bar: 2, baz: { bop: 3 } });
+    test.ok(!helpers.isLiteral('.11'), '".11" should not be a valid literal');
+    test.ok(!helpers.isLiteral('1.'), '"1." should not be a valid literal');
+    test.ok(!helpers.isLiteral('"foo'), '"foo" should not be a valid literal');
+    test.ok(!helpers.isLiteral('foo\''), '"foo" should not be a valid literal');
     test.done();
 };
 
-exports.merge = function (test) {
-    var a = { foo: 1, bar: 2 },
-        b = { foo: 2 },
-        out;
+exports.isStringLiteral = function (test) {
+    test.ok(helpers.isStringLiteral('"0.1"'), '"0.1" should be a valid literal');
+    test.ok(helpers.isStringLiteral('"foobar"'), '"foobar" should be a valid literal');
+    test.ok(helpers.isStringLiteral("'foobar'"), '\'foobar\' should be a valid literal');
 
-    out = helpers.merge(a, b);
-    test.deepEqual(out, { foo: 2, bar: 2 }, 'returns merged object');
-    test.deepEqual(a, { foo: 1, bar: 2 }, 'does not overwrite original object');
+    test.ok(!helpers.isStringLiteral('1'), '"1" should not be a valid literal');
+    test.ok(!helpers.isStringLiteral('.11'), '".11" should not be a valid literal');
+    test.ok(!helpers.isStringLiteral('1.'), '"1." should not be a valid literal');
+    test.ok(!helpers.isStringLiteral('"foo'), '"foo" should not be a valid literal');
+    test.ok(!helpers.isStringLiteral('foo\''), '"foo" should not be a valid literal');
+    test.done();
+};
 
+exports.isValidName = function (test) {
+    test.ok(helpers.isValidShortName('foobar'), '"foobar" should be a valid name');
+    test.ok(helpers.isValidShortName('$foobar'), '"$foobar" should be a valid name');
+    test.ok(helpers.isValidShortName('_foobar'), '"_foobar" should be a valid name');
+    test.ok(helpers.isValidShortName('foo$'), '"foo$" should be a valid name');
+
+    test.ok(!helpers.isValidShortName('0foobar'), '"0foobar" should not be a valid name');
+    test.ok(!helpers.isValidShortName('var'), '"var" should not be a valid name');
+    test.ok(!helpers.isValidShortName('__foo'), '"__foo" should not be a valid name');
+    test.done();
+};
+
+exports.isValidShortName = function (test) {
+    test.ok(helpers.isValidShortName('foobar'), '"foobar" should be a valid shortname');
+    test.ok(helpers.isValidShortName('$foobar'), '"$foobar" should be a valid shortname');
+    test.ok(helpers.isValidShortName('_foobar'), '"_foobar" should be a valid shortname');
+    test.ok(helpers.isValidShortName('foo$'), '"foo$" should be a valid shortname');
+
+    test.ok(!helpers.isValidShortName('0foobar'), '"0foobar" should not be a valid shortname');
+    test.ok(!helpers.isValidShortName('var'), '"var" should not be a valid shortname');
+    test.ok(!helpers.isValidShortName('__foo'), '"__foo" should not be a valid shortname');
+    test.done();
+};
+
+exports.isValidBlockName = function (test) {
+    test.ok(helpers.isValidBlockName('foobar'), '"foobar" should be a valid block name');
+    test.ok(helpers.isValidBlockName('foo_bar0'), '"foo_bar0" should be a valid block name');
+
+    test.ok(!helpers.isValidBlockName('_foobar'), '"_foobar" should not be a valid block name');
+    test.ok(!helpers.isValidBlockName('0foobar'), '"0foobar" should not be a valid block name');
+    test.ok(!helpers.isValidBlockName('foo$'), '"foo$" should not be a valid block name');
+
+    test.done();
+};
+
+exports.escaper = function (test) {
+    test.strictEqual(helpers.escaper('hi<foo> & "bar" \'baz\''), 'hi&lt;foo&gt; &amp; &quot;bar&quot; &#39;baz&#39;');
     test.done();
 };
