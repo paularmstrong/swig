c82f36d57ab99fa47a9733a98a9e66ddee83f36f
Paul Armstrong <paul@paularmstrongdesigns.com>
2011-09-11 20:03:21 -0700

    Allow filters on variables used for in `for` loops.

--- tests/tags.test.js
+++ tests/tags.test.js
@@ -36,3 +36,29 @@ exports.if = testCase({
         test.done();
     }
 });
+
+exports.for = testCase({
+    basic: function (test) {
+        swig.init({});
+
+        var tmpl8 = swig.fromString('{% for foo in bar %}{{ foo }}, {% endfor %}');
+        test.strictEqual(tmpl8.render({ bar: ['foo', 'bar', 'baz'] }), 'foo, bar, baz, ');
+        test.done();
+    },
+
+    index: function (test) {
+        swig.init({});
+
+        var tmpl8 = swig.fromString('{% for foo in bar %}{{ forloop.index }}{% endfor %}');
+        test.strictEqual(tmpl8.render({ bar: ['foo', 'bar', 'baz'] }), '012');
+        test.done();
+    },
+
+    'loop object allows filters': function (test) {
+        swig.init({});
+
+        var tmpl8 = swig.fromString('{% for foo in bar|reverse %}{{ foo }}{% endfor %}');
+        test.strictEqual(tmpl8.render({ bar: ['baz', 'bar', 'foo'] }), 'foobarbaz');
+        test.done();
+    }
+});
