16a0326f1e93c0425094859f1efcf58e025f7eed
Paul Armstrong <paul@paularmstrongdesigns.com>
2011-09-30 21:28:33 -0700

    move browser build to bash script, add pack underscore task

--- Makefile
+++ Makefile
@@ -1,55 +1,10 @@
-BROWSER_FILE = "dist/browser/swig.js"
-TEMP_FILE = "dist/.swig.js"
-
 all:
 	@npm install -d
 	@cp scripts/githooks/* .git/hooks/
 	@chmod -R +x .git/hooks/
 
 browser:
-	@rm -rf dist/browser
-	@mkdir -p dist/browser
-	@cat dist/header.js >> $(BROWSER_FILE)
-	@echo "swig = (function () {" >> $(BROWSER_FILE)
-	@echo "var swig = {}," >> $(BROWSER_FILE)
-	@echo "    dateformat = {}," >> $(BROWSER_FILE)
-	@echo "    filters = {}," >> $(BROWSER_FILE)
-	@echo "    helpers = {}," >> $(BROWSER_FILE)
-	@echo "    parser = {}," >> $(BROWSER_FILE)
-	@echo "    tags = {};" >> $(BROWSER_FILE)
-
-	@echo "(function (exports) {" >> $(BROWSER_FILE)
-	@cat index.js >> $(BROWSER_FILE)
-	@echo "})(swig);" >> $(BROWSER_FILE)
-
-	@echo "(function (exports) {" >> $(BROWSER_FILE)
-	@cat lib/dateformat.js >> $(BROWSER_FILE)
-	@echo "})(dateformat);" >> $(BROWSER_FILE)
-
-	@echo "(function (exports) {" >> $(BROWSER_FILE)
-	@cat lib/filters.js >> $(BROWSER_FILE)
-	@echo "})(filters);" >> $(BROWSER_FILE)
-
-	@echo "(function (exports) {" >> $(BROWSER_FILE)
-	@cat lib/helpers.js >> $(BROWSER_FILE)
-	@echo "})(helpers);" >> $(BROWSER_FILE)
-
-	@echo "(function (exports) {" >> $(BROWSER_FILE)
-	@cat lib/parser.js >> $(BROWSER_FILE)
-	@echo "})(parser);" >> $(BROWSER_FILE)
-
-	@echo "(function (exports) {" >> $(BROWSER_FILE)
-	@cat lib/tags.js >> $(BROWSER_FILE)
-	@echo "})(tags);" >> $(BROWSER_FILE)
-
-	@echo "return swig;" >> $(BROWSER_FILE)
-	@echo "})();" >> $(BROWSER_FILE)
-
-	@cp $(BROWSER_FILE) $(TEMP_FILE)
-	@sed "/require/d" <$(TEMP_FILE) > $(BROWSER_FILE)
-	@rm $(TEMP_FILE)
-
-	@node_modules/uglify-js/bin/uglifyjs $(BROWSER_FILE) > dist/browser/swig.min.js
+	@scripts/browser.sh
 
 test:
 	@node tests/speed.js
---
+++ scripts/browser.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+function package() {
+    if [[ $1 == 'pack' ]]; then
+        BROWSER_FILE="dist/browser/swig.pack.js"
+        TEMP_FILE="dist/.swig.pack.js"
+        MIN_FILE="dist/browser/swig.pack.min.js"
+    else
+        BROWSER_FILE="dist/browser/swig.js"
+        TEMP_FILE="dist/.swig.js"
+        MIN_FILE="dist/browser/swig.min.js"
+    fi
+
+    cat dist/header.js >> $BROWSER_FILE
+    echo "swig = (function () {" >> $BROWSER_FILE
+    echo "var swig = {}," >> $BROWSER_FILE
+    echo "dateformat = {}," >> $BROWSER_FILE
+    echo "filters = {}," >> $BROWSER_FILE
+    echo "helpers = {}," >> $BROWSER_FILE
+    echo "parser = {}," >> $BROWSER_FILE
+    echo "tags = {};" >> $BROWSER_FILE
+
+    if [[ $1 == 'pack' ]]; then
+        cat node_modules/underscore/underscore.js >> $BROWSER_FILE
+    fi
+
+    echo "(function (exports) {" >> $BROWSER_FILE
+    cat index.js >> $BROWSER_FILE
+    echo "})(swig);" >> $BROWSER_FILE
+
+    echo "(function (exports) {" >> $BROWSER_FILE
+    cat lib/dateformat.js >> $BROWSER_FILE
+    echo "})(dateformat);" >> $BROWSER_FILE
+
+    echo "(function (exports) {" >> $BROWSER_FILE
+    cat lib/filters.js >> $BROWSER_FILE
+    echo "})(filters);" >> $BROWSER_FILE
+
+    echo "(function (exports) {" >> $BROWSER_FILE
+    cat lib/helpers.js >> $BROWSER_FILE
+    echo "})(helpers);" >> $BROWSER_FILE
+
+    echo "(function (exports) {" >> $BROWSER_FILE
+    cat lib/parser.js >> $BROWSER_FILE
+    echo "})(parser);" >> $BROWSER_FILE
+
+    echo "(function (exports) {" >> $BROWSER_FILE
+    cat lib/tags.js >> $BROWSER_FILE
+    echo "})(tags);" >> $BROWSER_FILE
+
+    echo "return swig;" >> $BROWSER_FILE
+    echo "})();" >> $BROWSER_FILE
+
+    cp $BROWSER_FILE $TEMP_FILE
+    sed "/require/d" <$TEMP_FILE > $BROWSER_FILE
+    rm $TEMP_FILE
+
+    node_modules/uglify-js/bin/uglifyjs $BROWSER_FILE > $MIN_FILE
+}
+
+rm -rf dist/browser
+mkdir -p dist/browser
+package
+package "pack"
