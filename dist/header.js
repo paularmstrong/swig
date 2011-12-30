9b2f53704fe0641d38356742797c0345b51efa4f
Paul Armstrong <paul@paularmstrongdesigns.com>
2011-12-30 14:46:54 -0800

    add DateZ license to browser header, use link to underscore license

--- dist/header.js
+++ dist/header.js
@@ -1,3 +1,4 @@
 /*! Swig https://paularmstrong.github.com/swig | https://github.com/paularmstrong/swig/blob/master/LICENSE */
 /*! Cross-Browser Split 1.0.1 (c) Steven Levithan <stevenlevithan.com>; MIT License An ECMA-compliant, uniform cross-browser split method */
-/*! Underscore.js 1.1.7 (c) 2011 Jeremy Ashkenas, DocumentCloud Inc. Underscore is freely distributable under the MIT license. Portions of Underscore are inspired or borrowed from Prototype, Oliver Steele's Functional, and John Resig's Micro-Templating. For all details and documentation: http://documentcloud.github.com/underscore */
+/*! Underscore.js (c) 2011 Jeremy Ashkenas | https://github.com/documentcloud/underscore/blob/master/LICENSE */
+/*! DateZ (c) 2011 Tomo Universalis | https://github.com/TomoUniversalis/DateZ/blob/master/LISENCE */
