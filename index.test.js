09e57f5ac84f2eccf1138a5f3006ce5ad890ad41
Paul Armstrong <paul@paularmstrongdesigns.com>
2012-10-27 12:34:53 -0700

    convert index test to mocha

--- index.test.js
+++ index.test.js
@@ -1,177 +1,84 @@
-var swig = require('./index'),
-  testCase = require('nodeunit').testCase;
+var swig = require('./index');
 
-exports.init = testCase({
-  'custom filtersm': function (test) {
+describe('swig.init', function () {
+
+  it('accepts custom filters', function () {
     swig.init({ filters: {
       foo: function (input) {
         return 'bar';
       }
     }});
+    swig.compile('{{ asdf|foo }}')({ asdf: 'blah' }).should.equal('bar');
+  });
 
-    var tpl = swig.compile('{{ asdf|foo }}');
-    test.strictEqual(tpl({ asdf: 'blah' }), 'bar');
-    test.done();
-  },
-
-  'allowErrors = false': function (test) {
-    swig.init({
-      root: __dirname + '/tests/templates',
-      allowErrors: false
-    });
-    var tpl = swig.compileFile('foobar.html');
-    test.ok((/<pre>Error\: ENOENT, no such file or directory/i).test(tpl.render()), 'pushes a render function with the error');
-    tpl = swig.compileFile('includes_notfound.html');
-    test.ok((/<pre>Error\: ENOENT, no such file or directory/i).test(tpl.render()), 'renders the error when includes a file that is not found');
-
-    test.done();
-  },
-
-  'allowErrors = true': function (test) {
-    swig.init({ allowErrors: true });
-    test.throws(function () {
-      swig.compileFile('barfoo.html');
-    }, 'throws when allowErrors is true');
-
-    test.done();
-  },
-
-  'custom extensions': function (test) {
-    test.expect(1);
+  it('accepts custom extensions', function (done) {
     swig.init({
       allowErrors: true,
-      extensions: { foobar: function () { test.ok(true); } },
+      extensions: { foobar: function () { done(); } },
       tags: {
         foo: function (indent) {
           return '_ext.foobar();';
         }
       }
     });
+    swig.compile('{% foo %}')();
+  });
 
-    var tpl = swig.compile('{% foo %}');
-    tpl();
-    test.done();
-  }
+  describe('allowErrors', function () {
+    it('throws errors when true', function () {
+      swig.init({ allowErrors: true });
+      var fn = function () {
+        swig.compileFile('barfoo.html');
+      };
+      fn.should.throw();
+    });
+
+    it('does not throw when false, renders instead', function () {
+      swig.init({
+        root: __dirname + '/tests/templates',
+        allowErrors: false
+      });
+      swig.compileFile('foobar.html').render()
+        .should.match(/<pre>Error\: ENOENT, no such file or directory/i);
+      swig.compileFile('includes_notfound.html').render()
+        .should.match(/<pre>Error\: ENOENT, no such file or directory/i);
+    });
+  });
 });
 
-exports.compileFile = testCase({
-  setUp: function (callback) {
-    callback();
-  },
 
-  basic: function (test) {
+describe('swig.compileFile', function () {
+
+  it('compiles a template from a file', function () {
     swig.init({
       root: __dirname + '/tests/templates',
       allowErrors: true
     });
+    swig.compileFile('included_2.html').render({ array: [1, 1] })
+      .should.equal('2');
+  });
 
-    var tpl = swig.compileFile('included_2.html');
-    test.strictEqual('2', tpl.render({ array: [1, 1] }), 'from file is a-ok');
-    test.done();
-  },
 
-  'using context var with extends tag' : function (test) {
+  it('can use an absolute path', function () {
     swig.init({
       root: __dirname + '/tests/templates',
       allowErrors: true
     });
+    swig.compileFile('/' + __dirname + '/tests/templates/included_2.html').render({ array: [1, 1] })
+      .should.equal('2');
+  });
 
-    var tpl, r1, r2, r3, r4;
-
-    tpl = swig.compileFile('extends_dynamic.html');
-    r1 = tpl.render({baseTmpl: "extends_base.html"});
-    r2 = tpl.render({baseTmpl: "extends_base2.html"});
-    r3 = tpl.render({baseTmpl: "extends_base.html"});
-    r4 = tpl.render({baseTmpl: "extends_base2.html"});
-
-    test.strictEqual(r1, r3, "rendering the same template with the same context twice, should return identically.");
-    test.strictEqual(r2, r4, "rendering the same template with the same context twice, should return identically.");
-
-    test.notEqual(r1, r2, "these should not be equal, as they use different base templates.");
-
-    test.done();
-  },
-
-  'absolute path': function (test) {
-    swig.init({
-      root: __dirname + '/tests/templates',
-      allowErrors: true
-    });
-    var tpl = swig.compileFile('/' + __dirname + '/tests/templates/included_2.html');
-    test.strictEqual('2', tpl.render({ array: [1, 1] }), 'file from absolute path is a-ok');
-    test.done();
-  },
-
-  'throws on window': function (test) {
+  it('throws in a browser context', function () {
     swig.init({});
     global.window = true;
-    test.throws(function () {
+    var fn = function () {
       swig.compileFile('foobar');
-    });
+    };
+    fn.should.throw();
     delete global.window;
-    test.done();
-  },
+  });
 
-  'render without context': function (test) {
-    test.strictEqual(swig.compile('{% set foo = "foo" %}{{ foo }}')(), 'foo', 'this should not throw');
-    test.done();
-  }
+  it('can render without context', function () {
+    swig.compile('{% set foo = "foo" %}{{ foo }}')().should.equal('foo');
+  });
 });
-
-exports.Errors = testCase({
-  'allow - parse error': function (test) {
-    swig.init({ allowErrors: true });
-
-    test.throws(function () {
-      swig.compile('{% for foo in blah %}{% endif %}');
-    }, Error);
-    test.done();
-  },
-
-  'allow - compile error': function (test) {
-    swig.init({
-      allowErrors: true,
-      tags: { foo: function (indent) {
-        return 'blargh;';
-      }}
-    });
-
-    var tpl = swig.compile('{% foo %}');
-    test.throws(function () {
-      tpl({});
-    }, Error);
-    test.done();
-  },
-
-  'disallow - parse error': function (test) {
-    swig.init({});
-
-    test.doesNotThrow(function () {
-      swig.compile('{% for foo in bar %}{% endif %}');
-    }, Error);
-    test.done();
-  },
-
-  'disallow - compile error': function (test) {
-    swig.init({
-      tags: { foobar: function (indent) {
-        return 'blargh;';
-      }}
-    });
-
-    var tpl = swig.compile('{% foobar %}');
-    test.doesNotThrow(function () {
-      tpl({});
-    }, Error);
-    test.done();
-  }
-});
-
-exports['double-escape forward-slash'] = function (test) {
-  swig.init({});
-
-  var tpl = swig.compile('foobar\\/');
-  test.strictEqual(tpl({}), 'foobar\\/');
-
-  test.done();
-};
