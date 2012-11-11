bd2411ae46d867632a55644a7050792c425bd6f4
nka11 <nka11@nka.me>
2012-11-11 19:49:18 +0100

    adding multi-root setting. If param root is an array of strings, swig will search for the file in directories using the order specified in the array.

--- tests/node/swig.test.js
+++ tests/node/swig.test.js
@@ -59,7 +59,14 @@ describe('swig.compileFile', function () {
       expect(swig.compileFile('included_2.html').render({ array: [1, 1] }))
         .to.equal('2');
     });
-
+    it('accepts an array in root config', function () {
+      swig.init({
+        root: ["/", __dirname + '/templates'],
+        allowErrors: true
+      });
+      expect(swig.compileFile('included_2.html').render({ array: [1, 1] }))
+        .to.equal('2');
+    });
     it('can use an absolute path', function () {
       swig.init({
         root: __dirname + '/templates',
