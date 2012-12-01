8762f5fc6a81a0f9ab5be83d9bc53e5f322c8c6a
Paul Armstrong <paul@paularmstrongdesigns.com>
2012-12-01 10:04:05 -0800

    pretty header

--- views/js/swig.js
+++ views/js/swig.js
@@ -1,10 +1,2 @@
 (function () {
-  var $header = $('.header'),
-    scrolledClass = 'scrolled',
-    win = window;
-
-  $(win).on('scroll', function (e) {
-    console.log(document.scrollY)
-    $header.toggleClass(scrolledClass, (win.scrollY > 0));
-  });
 }());
