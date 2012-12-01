00943e9a13ffb8161f1a4701dadfef47762561b5
Paul Armstrong <paul@paularmstrongdesigns.com>
2012-12-01 10:53:34 -0800

    condense docs to single page

--- views/js/swig.js
+++ views/js/swig.js
@@ -1,2 +1,29 @@
 (function () {
+
+  var $sidebar = $('.sidenav'),
+    top = $sidebar.offset().top,
+    $links = $sidebar.find('a');
+
+  $(window).on('scroll', function (e) {
+    var scrollY = this.scrollY;
+    if (scrollY > top) {
+      $sidebar.addClass('fixed');
+    } else if (scrollY < top) {
+      $sidebar.removeClass('fixed');
+    }
+
+    $sidebar.find('.current').removeClass('current');
+    $links.each(function () {
+      var $this = $(this),
+        $section = $($this.attr('href'));
+      if ($section.offset().top <= scrollY) {
+        $sidebar.find('.current').removeClass('current');
+        $this.addClass('current');
+      } else {
+        $this.removeClass('current');
+      }
+    });
+    $sidebar.find('.current').parents('li').addClass('current');
+  });
+
 }());
