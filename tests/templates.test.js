1d482674a2b65405fbfd982e7fb0df8351d53f88
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-07-14 15:45:06 -0700

    basic inheritance using extends/blocks

---
+++ tests/templates.test.js
@@ -0,0 +1,31 @@
+var fs = require('fs'),
+  swig = require('../index.js'),
+  expect = require('expect.js'),
+  _ = require('lodash'),
+  Swig = swig.Swig;
+
+function isTest(f) {
+  return (/\.test\.html$/).test(f);
+}
+
+function isExpectation(f) {
+  return (/\.expectation\.html$/).test(f);
+}
+
+describe('Templates', function () {
+  var casefiles = fs.readdirSync(__dirname + '/cases/'),
+    tests = _.filter(casefiles, isTest),
+    expectations = _.filter(casefiles, isExpectation),
+    cases = _.groupBy(tests.concat(expectations), function (f) {
+      return f.split('.')[0];
+    });
+
+  _.each(cases, function (files, c) {
+    var test = _.find(files, isTest),
+      expectation = fs.readFileSync(__dirname + '/cases/' + _.find(files, isExpectation), 'utf8');
+
+    it(c, function () {
+      expect(swig.compileFile(__dirname + '/cases/' + test)()).to.equal(expectation);
+    });
+  });
+});
