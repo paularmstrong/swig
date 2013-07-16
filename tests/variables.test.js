58d541abbf1071e6037ff9b5c406d3605d08151d
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-07-15 21:38:02 -0700

    start of inner-token lexer

---
+++ tests/variables.test.js
@@ -0,0 +1,12 @@
+var swig = require('../index.js'),
+  expect = require('expect.js'),
+  _ = require('lodash'),
+  Swig = swig.Swig;
+
+describe('Variables', function () {
+  it('can be string and number literals', function () {
+    expect(swig.render('{{ "a" }}')).to.eql('a');
+    expect(swig.render('{{ 1 }}')).to.eql('1');
+    expect(swig.render('{{ 1.5 }}')).to.eql('1.5');
+  });
+});
