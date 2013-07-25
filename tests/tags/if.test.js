1c6e32c8f2198672a85175495ee85c1e0bca1385
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-07-25 16:51:07 -0700

    add more if tag tests, more arg parsing

--- tests/tags/if.test.js
+++ tests/tags/if.test.js
@@ -6,27 +6,62 @@ var swig = require('../../index.js'),
 var opts = {
   locals: {
     foo: 1,
-    bar: 0
+    bar: 0,
+    baz: [1]
   }
 };
 
 var cases = [
-  { code: 'foo > bar',    result: true },
-  { code: 'foo >= foo',   result: true },
-  { code: 'foo >= bar',   result: true },
-  { code: 'foo < bar',    result: false },
-  { code: 'bar <= bar',   result: true },
-  { code: 'foo <= bar',   result: false },
-  { code: 'foo !== bar',  result: true },
-  { code: 'foo != bar',   result: true }
+  { code: 'foo',                result: true },
+  { code: 'true',               result: true },
+  { code: 'false',              result: false },
+  { code: 'foo > bar',          result: true },
+  { code: 'foo gt bar',         result: true },
+  { code: 'foo >= foo',         result: true },
+  { code: 'foo gte bar',        result: true },
+  { code: 'foo >= bar',         result: true },
+  { code: 'foo < bar',          result: false },
+  { code: 'foo lt bar',         result: false },
+  { code: 'bar <= bar',         result: true },
+  { code: 'foo <= bar',         result: false },
+  { code: 'foo lte bar',        result: false },
+  { code: 'foo !== bar',        result: true },
+  { code: 'foo != bar',         result: true },
+  { code: '!foo',               result: false },
+  { code: 'not foo',            result: false },
+  { code: '!bar',               result: true },
+  { code: 'not bar',            result: true },
+  { code: 'foo in baz',         result: false },
+  { code: 'bar in baz',         result: true },
+  { code: '1 < 0',              result: false },
+  { code: '"a" === "a"',        result: true },
+  { code: '1 === foo',          result: true },
+  { code: '1 === bar',          result: false },
+  { code: 'true && false',      result: false },
+  { code: '0 || (bar && foo)',  result: false },
 ];
 
-describe('Tag: {% if ', function () {
+describe('Tag: if', function () {
 
   _.each(cases, function (c) {
-    it(c.code + ' %}', function () {
+    it('{% if ' + c.code + ' %}', function () {
       expect(swig.render('{% if ' + c.code + '%}pass{% endif %}', opts))
         .to.equal(c.result ? 'pass' : '');
+    });
+  });
+
+  it('throws on bad logic', function () {
+    var baddies = [
+      '{% if && foo %}{% endif %}',
+      '{% if foo && %}{% endif %}',
+      '{% if foo > %}{% endif %}',
+      '{% if foo ! %}{% endif %}',
+      '{% if (foo %}{% endif %}'
+    ];
+    _.each(baddies, function (b) {
+      expect(function () {
+        swig.render(b);
+      }).to.throwError();
     });
   });
 
