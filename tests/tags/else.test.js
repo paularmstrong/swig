ebb8dd501085107078f7cf1c7ca300c9bc8fd551
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-07-25 19:22:15 -0700

    add elseif tag, alias elif

--- tests/tags/else.test.js
+++ tests/tags/else.test.js
@@ -13,7 +13,7 @@ describe('Tag: else', function () {
 
   _.each(cases, function (c) {
     it('{% if ' + c.code + ' %}{% else %}', function () {
-      expect(swig.render('{% if ' + c.code + '%}{% else %}pass{% endif %}'))
+      expect(swig.render('{% if ' + c.code + ' %}{% else %}pass{% endif %}'))
         .to.equal(c.result ? 'pass' : '');
     });
   });
@@ -22,6 +22,30 @@ describe('Tag: else', function () {
     expect(function () {
       swig.render('{% else %}foo');
     }).to.throwError(/Unexpected tag "else" on line 1\./);
+  });
+
+});
+
+describe('Tag: elseif, elif', function () {
+
+  _.each(cases, function (c) {
+    it('{% if ' + (!c.code) + ' %}{% elseif ' + c.code + ' %}', function () {
+      expect(swig.render('{% if ' + (!c.code) + ' %}{% elseif ' + c.code + ' %}pass{% endif %}'))
+        .to.equal(!c.result ? 'pass' : '');
+      expect(swig.render('{% if ' + (!c.code) + ' %}{% elif ' + c.code + ' %}pass{% endif %}'))
+        .to.equal(!c.result ? 'pass' : '');
+    });
+  });
+
+  it('{% if false %}{% elseif foo > 1 %}', function () {
+    expect(swig.render('{% if false %}{% elseif foo > 1 %}pass{% endif %}', { locals: { foo: 5 }}))
+      .to.equal('pass');
+  });
+
+  it('must be within an {% if %}', function () {
+    expect(function () {
+      swig.render('{% elseif %}foo');
+    }).to.throwError(/Unexpected tag "elseif" on line 1\./);
   });
 
 });
