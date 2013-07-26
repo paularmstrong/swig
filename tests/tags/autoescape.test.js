cedef39b8278c1c02dacdabcdf7800d530b4d186
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-07-25 20:35:31 -0700

    add autoescape tag

---
+++ tests/tags/autoescape.test.js
@@ -0,0 +1,31 @@
+var swig = require('../../index.js'),
+  expect = require('expect.js'),
+  _ = require('lodash'),
+  Swig = swig.Swig;
+
+var n = new Swig(),
+  oDefaults = n.options;
+
+function resetOptions() {
+  swig.setDefaults(oDefaults);
+  swig.invalidateCache();
+}
+
+describe('Tag: autoescape', function () {
+
+  beforeEach(resetOptions);
+  afterEach(resetOptions);
+
+  it('{% autoescape true %} turns escaping on', function () {
+    swig.setDefaults({ autoescape: false });
+    expect(swig.render('{% autoescape true %}{{ "<foo>" }}{% endautoescape %}{{ "<bar>" }}'))
+      .to.equal('&lt;foo&gt;<bar>');
+  });
+
+  it('{% autoescape false %} turns escaping off', function () {
+    swig.setDefaults({ autoescape: true });
+    expect(swig.render('{% autoescape false %}{{ "<foo>" }}{% endautoescape %}{{ "<bar>" }}'))
+      .to.equal('<foo>&lt;bar&gt;');
+  });
+
+});
