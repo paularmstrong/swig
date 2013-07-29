207818fb1b9fad1d8590a65a0be9af8a17d9e985
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-07-29 15:00:28 -0700

    add filter tag

---
+++ tests/tags/filter.test.js
@@ -0,0 +1,25 @@
+var swig = require('../../index.js'),
+  expect = require('expect.js'),
+  _ = require('lodash'),
+  Swig = swig.Swig;
+
+var cases = [
+  { c: 'upper', e: 'PASS' },
+  { c: 'default("foobar")', e: 'pass' },
+  { c: 'replace("s", "d", "g")', e: 'padd' }
+];
+
+describe('Tag: filter', function () {
+  _.each(cases, function (c) {
+    it('{% filter ' + c.c + ' %}', function () {
+      expect(swig.render('{% filter ' + c.c + '%}pass{% endfilter %}', {}))
+        .to.equal(c.e);
+    });
+  });
+
+  it('Throws on non-existent filter', function () {
+    expect(function () {
+      swig.render('{% filter foobar %}{% endfilter %}');
+    }).to.throwError(/Filter \"foobar\" does not exist on line 1\./);
+  });
+});
