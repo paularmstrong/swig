872ea067f00ea95a48b8b3f78b63e07b6f1c585c
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-07-29 15:18:39 -0700

    spaceless tag

---
+++ tests/tags/spaceless.test.js
@@ -0,0 +1,23 @@
+var swig = require('../../index.js'),
+  expect = require('expect.js'),
+  _ = require('lodash'),
+  Swig = swig.Swig;
+
+var cases = [
+  { c: '{% spaceless %} <p> foo </p> <p>bar</p> {% endspaceless %}', e: '<p> foo </p><p>bar</p>' },
+  { c: '{% spaceless %}{% if true %}<p></p> <p></p>{% endif %}{% endspaceless %}', e: '<p></p><p></p>' }
+];
+
+describe('Tag: spaceless', function () {
+  _.each(cases, function (c) {
+    it(c.c, function () {
+      expect(swig.render(c.c)).to.equal(c.e);
+    });
+  });
+
+  it('Throws on tokens', function () {
+    expect(function () {
+      swig.render('{% spaceless foobar %}{% endfilter %}');
+    }).to.throwError(/Unexpected token \"foobar\" on line 1\./);
+  });
+});
