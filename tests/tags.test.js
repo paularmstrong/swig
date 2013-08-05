6c2529da9c9f54773e07ab537aa53a71e7d6416d
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-08-05 16:48:45 -0700

    add test for custom tag adding

--- tests/tags.test.js
+++ tests/tags.test.js
@@ -15,4 +15,17 @@ describe('Tags', function () {
       swig.render('\n{% if foo %}\n  asdf\n{% endfoo %}');
     }).to.throwError(/Unexpected end of tag "foo" on line 4\./);
   });
+
+  it('can be added', function () {
+    function parse(str, line, parser, types) {
+      return true;
+    }
+    function compile(compiler, args, content) {
+      return compiler(content) + '\n' +
+        '_output += " tortilla!"';
+    }
+    swig.addTag('tortilla', parse, compile, true);
+    expect(swig.render('{% tortilla %}flour{% endtortilla %}'))
+      .to.equal('flour tortilla!');
+  });
 });
