513f1190858822da7c302bbef11bdd15c315d22c
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-08-04 20:31:58 -0700

    coverage: import 100%

---
+++ tests/tags/import.test.js
@@ -0,0 +1,15 @@
+var swig = require('../../lib/swig'),
+  expect = require('expect.js'),
+  _ = require('lodash'),
+  Swig = swig.Swig;
+
+describe('Tag: import', function () {
+  it('throws on bad arguments', function () {
+    expect(function () {
+      swig.render('{% import "foo" "bar" %}');
+    }).to.throwError(/Unexpected string "bar" on line 1\./);
+    expect(function () {
+      swig.render('{% import bar %}');
+    }).to.throwError(/Unexpected variable "bar" on line 1\./);
+  });
+});
