40a5401f47b829f0c2f9c924b36cfffba2ceed14
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-08-04 20:26:44 -0700

    coverage: filter tag 100%

--- tests/tags/filter.test.js
+++ tests/tags/filter.test.js
@@ -6,18 +6,22 @@ var swig = require('../../lib/swig'),
 var cases = [
   { c: 'upper', e: 'PASS' },
   { c: 'default("foobar")', e: 'pass' },
-  { c: 'replace("s", "d", "g")', e: 'padd' }
+  { c: 'replace("s", "d", "g")', e: 'padd' },
+  { c: 'default(fn("foo"))', e: 'pass' },
+  { c: 'default(foo)', e: 'pass' }
 ];
 
 describe('Tag: filter', function () {
   _.each(cases, function (c) {
     it('{% filter ' + c.c + ' %}', function () {
-      expect(swig.render('{% filter ' + c.c + '%}pass{% endfilter %}', {}))
+      expect(swig.render('{% filter ' + c.c + '%}pass{% endfilter %}', { locals: {
+        fn: function (input) { return input; }
+      }}))
         .to.equal(c.e);
     });
   });
 
-  it('Throws on non-existent filter', function () {
+  it('throws on non-existent filter', function () {
     expect(function () {
       swig.render('{% filter foobar %}{% endfilter %}');
     }).to.throwError(/Filter \"foobar\" does not exist on line 1\./);
