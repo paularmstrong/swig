3fea14d2a87a3706c742ab3e2c9646498932a31a
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-08-19 10:46:48 -0700

    preserve forward-slashes in text chunks. Fixes gh-285

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -209,3 +209,10 @@ describe('swig.renderFile', function () {
     });
   });
 });
+
+describe('Regressions', function () {
+  it('gh-285: preserves forward-slashes in text', function () {
+    expect(swig.render('foo\\ blah \\ and stuff'))
+      .to.equal('foo\\ blah \\ and stuff');
+  });
+});
