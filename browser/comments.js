1730a3356b869cf5cdcbaf7e86fcb7251bcdbeae
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-08-23 19:08:16 -0700

    ensure comments are preserved in minified source

--- browser/comments.js
+++ browser/comments.js
@@ -1,2 +1,2 @@
-/*! Swig v1.0.0-pre3 | https://paularmstrong.github.com/swig | https://github.com/paularmstrong/swig/blob/master/LICENSE */
-/*! DateZ (c) 2011 Tomo Universalis | https://github.com/TomoUniversalis/DateZ/blob/master/LISENCE */
+/*! Swig v1.0.0-pre3 | https://paularmstrong.github.com/swig | @license https://github.com/paularmstrong/swig/blob/master/LICENSE */
+/*! DateZ (c) 2011 Tomo Universalis | @license https://github.com/TomoUniversalis/DateZ/blob/master/LISENCE */
