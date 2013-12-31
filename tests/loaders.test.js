4bee340fb63cec757f05749777658c2c11a57025
Marat Khabibullin <maratfm@gmail.com>
2013-12-31 11:45:10 +0400

    added test with include

--- tests/loaders.test.js
+++ tests/loaders.test.js
@@ -7,12 +7,27 @@ var swig = require('../lib/swig'),
 
 
 describe('loaders', function () {
-  it('is working', function () {
+  it('is working with extends', function () {
     var templates, s, html;
 
     templates = {
       'layout.html': '<html>{% block content %}{% endblock %}</html>',
       'page.html': '{% extends "layout.html" %}{% block content %}Hello {{ name }}!{% endblock %}'
+    };
+
+    s = new Swig({ loader: new loaders.MemoryLoader(templates) });
+
+    html = s.renderFile('page.html', {name: 'world'});
+
+    expect(html).to.equal('<html>Hello world!</html>');
+  });
+
+  it('is working with include', function () {
+    var templates, s, html;
+
+    templates = {
+      'page.html': '<html>{% include "content.html" %}</html>',
+      'content.html': 'Hello {{ name }}!'
     };
 
     s = new Swig({ loader: new loaders.MemoryLoader(templates) });
