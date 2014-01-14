d53866fdc96433864b88ccd9707431dcbdc62645
Ajay Sabhaney <ajay@mothercreative.com>
2014-01-13 17:18:13 -0700

    Added bin tests for compliling & running w/ custom filters & tags

---
+++ tests/bin.filters.js
@@ -0,0 +1,3 @@
+exports.benice = function (input) {
+  return input + ' please!';
+};
---
+++ tests/bin.tags.js
@@ -0,0 +1,15 @@
+
+function parse(str, line, parser, types) {
+  return true;
+}
+
+function compile(compiler, args, content) {
+  return compiler(content) + '\n' +
+    '_output += " tortilla!"';
+}
+
+exports.tortilla = {
+  parse: parse,
+  compile: compile,
+  ends: true
+};
