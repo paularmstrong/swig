ef1c63175c986ddd80d04e13ac2bc26e0a0cbce6
Joe Ferraro <ferraro.joseph@gmail.com>
2014-11-07 12:27:39 -0500

    fixes safe mode check, adds loophole dependency, adds safe mode spec

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -115,6 +115,16 @@ describe('options', function () {
     });
   });
 
+  describe('safeMode', function () {
+    it('can be set as default', function () {
+      swig.setDefaults({ safeMode: true, locals: { a: 1, b: 2 }});
+      var tpl = '{{ a }}{{ b }}{{ c }}';
+      expect(swig.compile(tpl)({ c: 3 })).to.equal('123');
+      expect(swig.compile(tpl, { locals: { c: 3 }})()).to.equal('123');
+      expect(swig.render(tpl, { locals: { c: 3 }})).to.equal('123');
+    });
+  });
+
   describe('cache', function () {
     it('can be falsy', function () {
       var s = new Swig({ cache: false });
