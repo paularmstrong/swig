69591e0fbfcca608ceefe9e796cafc329d4d1be6
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2015-01-28 17:02:04 +0100

    Include array of paths test use cases.
    Passing and working.

--- tests/tags/include.test.js
+++ tests/tags/include.test.js
@@ -23,4 +23,28 @@ describe('Tag: include', function () {
       }).to.throwError(/Expected "missing" on line 1 but found "foobar"\./);
     });
   });
+
+  describe('{% include [ "file", "file2" ] ignore missing %}', function () {
+    it('does load "file" because it exists', function () {
+      var s = new swig.Swig({loader: swig.loaders.memory({ '/foo/file': 'file' }, '/foo')});
+      
+      expect( s.render( '{% include [ "file", "file2" ] %}' ) )
+      .to
+      .equal('file');
+    });
+
+    it('does load "file2" because "file" do not exist', function () {
+      var s = new swig.Swig({loader: swig.loaders.memory({ '/foo/file2': 'file2' }, '/foo')});
+
+      expect( s.render( '{% include [ "file", "file2" ] %}' ) )
+      .to
+      .equal('file2');
+    });
+
+    it('does load nothing because both do not exist', function () {
+      expect( swig.render('{% include [ "file", "file2" ] %}', { filename: '/foo' }) )
+      .to
+      .equal('');
+    });
+  });
 });
